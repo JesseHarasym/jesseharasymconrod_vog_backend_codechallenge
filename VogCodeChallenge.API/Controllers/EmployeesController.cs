314cb70dc19be288fa2e894dc294698f143179bb
Jesse Harasym <51863533+JesseHarasym@users.noreply.github.com>
2022-01-19 19:04:05 -0700

    Added restful api endpoints and refactored how we get the employee data..

---
+++ VogCodeChallenge.API/Controllers/EmployeesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using VogCodeChallenge.API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VogCodeChallenge.API
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        EmployeeSeedData _mockData = new EmployeeSeedData();
+
+        // GET api/employees
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return _mockData.GetAll();
+        }
+
+        // GET api/employees/{departmentId}
+        [HttpGet("{departmentId}")]
+        public IList<Employee> Get(int departmentId)
+        {
+            // TODO: change to grab only the employees with the specified dept id..
+            return _mockData.ListAll();
+        }
+    }
+}
---
+++ VogCodeChallenge.API/Models/EmployeeSeedData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace VogCodeChallenge.API.Models
+{
+    public class EmployeeSeedData
+    {
+        private List<Employee> _employeeList = new List<Employee>()
+        {
+            new Employee("Josh", "Adams", "123 Center Way", "Office Manager"),
+            new Employee("Alfred", "Old", "1927 Carrot Lane", "Head of Sales"),
+            new Employee("Johnny", "Carson", "59 Near The Moon", "VP of Marketing"),
+            new Employee("Checkers", "McMaster", "49 Driving St", "Consultant")
+        };
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _employeeList;
+        }
+
+        public IList<Employee> ListAll()
+        {
+            return _employeeList;
+        }
+    }
+}
