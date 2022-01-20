be5a8ca293c86d0ca65c7da44486cc5f47deb57e
Jesse Harasym <51863533+JesseHarasym@users.noreply.github.com>
2022-01-19 20:36:42 -0700

    addition of question 8 answer for our testmodule switch statement..

--- VogCodeConsole/QuestionClass.cs
+++ VogCodeConsole/QuestionClass.cs
@@ -10,5 +10,26 @@ namespace VogCodeConsole
             "Jeffrey",
             "John",
         };
+
+
+        // Method for Question #8
+        public static object TESTModule(object value)
+        {
+            switch (value)
+            {
+                case int n when (n <= 4 && n >= 1):
+                    return n *= 2;
+                case int n when (n > 4):
+                    return n *= 3;
+                case int n when (n < 1):
+                    throw new System.ArgumentOutOfRangeException("TESTModule parameter value is below 1");
+                case float n when (n == 1.0f || n == 2.0f):
+                    return 3.0f;
+                case string s:
+                    return s.ToUpper();
+                default:
+                    return value;
+            }
+        }
     }
 }
