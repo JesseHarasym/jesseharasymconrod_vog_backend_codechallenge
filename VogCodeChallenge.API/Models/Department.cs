23c55bfc3b4a67374675c8973f510f3238ebd9fa
Jesse Harasym <51863533+JesseHarasym@users.noreply.github.com>
2022-01-19 18:45:12 -0700

    Added basic model for employee and departments

---
+++ VogCodeChallenge.API/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace VogCodeChallenge.API.Models
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        private List<Employee> _employeeList = new List<Employee>();
+
+    }
+}
---
+++ VogCodeChallenge.API/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace VogCodeChallenge.API.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public Employee(string firstName, string lastName, string address, string jobTitle)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            JobTitle = jobTitle;
+        }
+    }
+}
