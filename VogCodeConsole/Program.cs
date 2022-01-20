05ac2bf565d0baa2a11f0abc45146507444606df
Jesse Harasym <51863533+JesseHarasym@users.noreply.github.com>
2022-01-19 20:33:03 -0700

    re-addition of question 7 code since the console app folder was missing..

---
+++ VogCodeConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VogCodeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // I would usually use method #2, for situations like this,
+            // but since it may be considered a loop I showed both..
+
+            // method #1
+            Console.WriteLine(string.Join(Environment.NewLine, QuestionClass.NameList));
+
+            // method #2
+            //QuestionClass.NameList.ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ VogCodeConsole/QuestionClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace VogCodeConsole
+{
+    public static class QuestionClass
+    {
+        public static List<string> NameList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+    }
+}
