569397caafc5daa85117cd34f930646ebfca866f
Jesse Harasym <51863533+JesseHarasym@users.noreply.github.com>
2022-01-19 18:46:49 -0700

    Added two methods for getting employee data and the test data

--- VogCodeChallenge.API/Models/Department.cs
+++ VogCodeChallenge.API/Models/Department.cs
@@ -8,7 +8,22 @@ namespace VogCodeChallenge.API.Models
 
         public string Address { get; set; }
 
-        private List<Employee> _employeeList = new List<Employee>();
+        private List<Employee> _employeeList = new List<Employee>()
+        {
+            new Employee("Josh", "Adams", "123 Center Way", "Office Manager"),
+            new Employee("Alfred", "Old", "1927 Carrot Lane", "Head of Sales"),
+            new Employee("Johnny", "Carson", "59 Near The Moon", "VP of Marketing"),
+            new Employee("Checkers", "McMaster", "49 Driving St", "Consultant")
+        };
 
+        public IEnumerable<Employee> GetAll()
+        {
+            return _employeeList;
+        }
+
+        public IList<Employee> ListAll()
+        {
+            return _employeeList;
+        }
     }
 }
